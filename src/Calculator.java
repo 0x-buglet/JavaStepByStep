3ee55ea2d243b5eec78c55a0bd58d47fab7a7cec
0x-buglet <ksjsooup@gmail.com>
2026-01-20 22:50:42 +0900

    feat: complete arithmetic operations and refactor test suite

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,17 @@
 public class Calculator {
-	// [ ver 2 ] 나누기 기능 추가
+	// [ ver 3 ] 뺄셈, 곱셈 기능 추가
 	public long add(int a, int b) {
 		return (long) a + b;
 	}
 	
+	public long subtract(int a, int b) {
+		return (long) a - b;
+	}
+
+	public long multiply(int a, int b) {
+		return (long) a * b;
+	}
+
 	public double divide(int a, int b) {
 		// [ 가드 절 ] 나누는 수가 0이면 미리 처리하기
 		if (b == 0) {
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -3,6 +3,8 @@ public class CalculatorTest {
 		Calculator calc = new Calculator();		
 		
 		testAdd(calc);
+		testSubtract(calc);
+		testMultiply(calc);
 		testDivide(calc);
 
 		System.out.println("✅ 모든 기능 테스트 성공적으로 마침!");
@@ -15,11 +17,24 @@ public class CalculatorTest {
      		expect(calc.add(Integer.MAX_VALUE, 1), (long)Integer.MAX_VALUE + 1, "MAX+1");
 	}
 
+	static void testSubtract(Calculator calc) {
+		System.out.println("--- [ Sudtract Test ] ---");
+		expect(calc.subtract(10, 5), 5L, "10-5");
+		expect(calc.subtract(5, 10), -5L, "5-10");
+	}
+
+	static void testMultiply(Calculator calc) {
+		System.out.println("--- [Multiply Test] ---");
+		expect(calc.multiply(10, 20), 200L, "10*20");
+		expect(calc.multiply(100000, 100000), 10000000000L, "Large Multiplication");
+	}
+
 	static void testDivide(Calculator calc) {
 		System.out.println("--- [Divide Test] ---");
-        	expect(calc.divide(10, 2), 5.0, "10 / 2");
-        	expect(calc.divide(5, 2), 2.5, "5 / 2");
-        	expect(calc.divide(1, 3), 1.0/3.0, "1 / 3");
+        	expect(calc.divide(10, 2), 5.0, "10/2");
+        	expect(calc.divide(5, 2), 2.5, "5/2");
+        	expect(calc.divide(1, 3), 1.0/3.0, "1/3");
+		expect(calc.divide(10, 0), 0.0, "Divide by Zero");
 	}
 
 	static void expect(long actual, long expected, String name) {
